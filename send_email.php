e5bc447974b8df2bf51ff3c59e6df09a0f14a3ed
Aparna-Rama <145258252+Aparna-Rama@users.noreply.github.com>
2024-05-25 19:19:24 +0530

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = filter_var(trim($_POST["name"]), FILTER_SANITIZE_STRING);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    if (empty($name) || empty($email) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Please complete the form and try again.";
+        exit;
+    }
+
+    $recipient = "raparna678@gmail.com";
+    $subject = "New contact from $name";
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    $email_headers = "From: $name <$email>";
+
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    echo "There was a problem with your submission, please try again.";
+}
+?>
